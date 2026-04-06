d6d4cf0fca782c9bf2ce19bc81b62463ed2f310c
brandaoplaster <brandaoplaster@gmail.com>
2026-04-06 08:47:52 -0300

    test: Add tests for user repository

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,12 +10,22 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 alias ApiTokenPool.Repo
+alias ApiTokenPool.Accounts.User
 alias ApiTokenPool.Tokens.Token
 
 Repo.transaction(fn ->
   Enum.each(1..100, fn _ ->
     %Token{}
     |> Token.changeset(%{})
+    |> Repo.insert!()
+  end)
+end)
+
+
+Repo.transaction(fn ->
+  Enum.each(1..105, fn _ ->
+    %User{}
+    |> User.changeset(%{name: Faker.Person.name()})
     |> Repo.insert!()
   end)
 end)
