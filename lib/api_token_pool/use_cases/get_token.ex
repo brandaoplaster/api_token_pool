9733aeb1f749a9ec04369768d9249987263d942c
brandaoplaster <brandaoplaster@gmail.com>
2026-04-05 22:31:38 -0300

    feat: Adjust get token to validate UUID

--- lib/api_token_pool/use_cases/get_token.ex
+++ lib/api_token_pool/use_cases/get_token.ex
@@ -1,7 +1,16 @@
 defmodule ApiTokenPool.UseCases.GetToken do
   alias ApiTokenPool.Repositories.TokenRepository
 
-  def execute(id) do
+  def execute(id) when is_binary(id) do
+    case Ecto.UUID.cast(id) do
+      {:ok, _} -> fetch(id)
+      :error -> {:error, :invalid_uuid}
+    end
+  end
+
+  def execute(_id), do: {:error, :invalid_uuid}
+
+  defp fetch(id) do
     case TokenRepository.get(id) do
       nil -> {:error, :not_found}
       token -> {:ok, token}
