23d9711e47fe2ab172b2955f65195c9ac88a9dea
brandaoplaster <brandaoplaster@gmail.com>
2026-04-05 21:02:25 -0300

    chore: Add status to token

--- priv/repo/migrations/20260405135415_create_tokens.exs
+++ priv/repo/migrations/20260405135415_create_tokens.exs
@@ -1,15 +1,26 @@
 defmodule ApiTokenPool.Repo.Migrations.CreateTokens do
   use Ecto.Migration
 
-  def change do
+  def up do
+    execute """
+    CREATE TYPE token_status AS ENUM ('available', 'allocated', 'blocked')
+    """
+
     create table(:tokens, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :allocated_at, :utc_datetime
+      add :status, :token_status, default: "available", null: false
       add :user_id, references(:users, on_delete: :nothing, type: :binary_id)
 
       timestamps(type: :utc_datetime)
     end
 
     create index(:tokens, [:user_id])
+    create index(:tokens, [:status])
+  end
+
+  def down do
+    drop table(:tokens)
+    execute "DROP TYPE token_status"
   end
 end
