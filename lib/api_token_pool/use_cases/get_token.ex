0f78cf9a1989ba6c7b746c6233b8c78e3b1d1e73
brandaoplaster <brandaoplaster@gmail.com>
2026-04-06 01:47:21 -0300

    feat: Add execute_history to get token

--- lib/api_token_pool/use_cases/get_token.ex
+++ lib/api_token_pool/use_cases/get_token.ex
@@ -1,5 +1,5 @@
 defmodule ApiTokenPool.UseCases.GetToken do
-  alias ApiTokenPool.Repositories.TokenRepository
+  alias ApiTokenPool.Repositories.{TokenRepository, UsageHistoryRepository}
 
   def execute(id) when is_binary(id) do
     case Ecto.UUID.cast(id) do
@@ -9,6 +9,18 @@ defmodule ApiTokenPool.UseCases.GetToken do
   end
 
   def execute(_id), do: {:error, :invalid_uuid}
+
+  def execute_history(id) when is_binary(id) do
+    with {:ok, _} <- Ecto.UUID.cast(id),
+         token when not is_nil(token) <- TokenRepository.get(id) do
+      {:ok, UsageHistoryRepository.list_by_token(token.id)}
+    else
+      :error -> {:error, :invalid_uuid}
+      nil -> {:error, :not_found}
+    end
+  end
+
+  def execute_history(_id), do: {:error, :invalid_uuid}
 
   defp fetch(id) do
     case TokenRepository.get(id) do
