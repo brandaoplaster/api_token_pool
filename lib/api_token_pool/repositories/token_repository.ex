8e100e1b09be6c412f91ab1aaed8d1449c003553
brandaoplaster <brandaoplaster@gmail.com>
2026-04-06 01:50:26 -0300

    feat: Add functions for token allocation in repository

--- lib/api_token_pool/repositories/token_repository.ex
+++ lib/api_token_pool/repositories/token_repository.ex
@@ -1,4 +1,6 @@
 defmodule ApiTokenPool.Repositories.TokenRepository do
+  import Ecto.Query, warn: false
+
   alias ApiTokenPool.Repo
   alias ApiTokenPool.Tokens.Token
 
@@ -8,5 +10,43 @@ defmodule ApiTokenPool.Repositories.TokenRepository do
 
   def get(id) do
     Repo.get(Token, id)
+  end
+
+  def allocate(token, user_id) do
+    token
+    |> Token.allocate_changeset(%{
+      user_id: user_id,
+      allocated_at: DateTime.truncate(DateTime.utc_now(), :second)
+    })
+    |> Repo.update()
+  end
+
+  def release(token) do
+    token
+    |> Token.release_changeset()
+    |> Repo.update()
+  end
+
+  def get_available do
+    Token
+    |> where([t], t.status == :available)
+    |> limit(1)
+    |> lock("FOR UPDATE SKIP LOCKED")
+    |> Repo.one()
+  end
+
+  def release_oldest do
+    token =
+      Token
+      |> where([t], t.status == :allocated)
+      |> order_by([t], asc: t.allocated_at)
+      |> limit(1)
+      |> lock("FOR UPDATE SKIP LOCKED")
+      |> Repo.one()
+
+    case token do
+      nil -> {:error, :no_tokens_available}
+      token -> token |> Token.release_changeset() |> Repo.update()
+    end
   end
 end
